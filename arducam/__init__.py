aefa828afe09c8302e9d48a8d4afea225ef2f468
Hongquan Li <hqlisu@gmail.com>
2026-03-28 01:19:47 -0700

    feat: package exports for library usage

---
+++ arducam/__init__.py
@@ -0,0 +1,11 @@
+from arducam.camera import ArducamCamera, RESOLUTION_FPS_TABLE
+from arducam.recorder import VideoRecorder, RecordingFormat
+from arducam.utils import save_capture
+
+__all__ = [
+    "ArducamCamera",
+    "RESOLUTION_FPS_TABLE",
+    "VideoRecorder",
+    "RecordingFormat",
+    "save_capture",
+]
